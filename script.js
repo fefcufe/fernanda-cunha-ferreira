eed4c428d095f9503b2fa843ae13d4590cf2f33a
Fernanda Ferreira <fefcufe@gmail.com>
2026-06-14 14:21:53 -0300

    adiciona formulario para enviar mensagem e faz as validacoes usando js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+const form = document.getElementById("formulario-contato");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); //Impede o envio do formulario antes de ele ser validado
+
+    const nome = document.getElementById("nome").value.trim(); //captura o valor de nome e remove espaços em branco
+    const email = document.getElementById("email").value.trim(); //captura o valor de email e remove espaços em branco
+    const mensagem = document.getElementById("mensagem").value.trim(); //captura o valor de mensagem e remove espaços em branco
+
+    if (nome === "" || email === "" || mensagem === "") {
+        alert("Preencha todos os campos.");
+        return;
+    } //validaçao se campos estão preenchidos
+
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // cria expressao regular que define o formato de um email válido
+
+    if (emailValido.test(email) === false) {
+        alert("Digite um email válido.");
+        return;
+    } // validacao por meio de regex para verificar se o email é válido
+
+    alert("Formulário enviado com sucesso!");
+
+    form.submit(); //envia a mensagem caso todas as validaçoes tenham sido realizadas com sucesso
+
+});
